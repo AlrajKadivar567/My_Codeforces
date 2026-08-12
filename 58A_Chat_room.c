35e97680fd4dc8ae8c19a9cb08f756c4cb8ad80b
AlrajKadivar567 <alrajkadiwar567@gmail.com>
2026-08-12 08:05:47 +0530

    Sync Codeforces: 58A - Chat room [GNU C11]

---
+++ 58A_Chat_room.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main() {
+    char s[101];
+    scanf("%s", s);
+ 
+    char target[] = "hello";
+    int j = 0;
+ 
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (s[i] == target[j]) {
+            j++;
+        }
+        if (j == 5) {
+            break;
+        }
+    }
+ 
+    if (j == 5)
+        printf("YES\n");
+    else
+        printf("NO\n");
+ 
+    return 0;
+}
